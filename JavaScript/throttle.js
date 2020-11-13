d426d13f8292d137842099d908e47a9ebd0848c8
npfive <v_junxing@tencent.com>
2020-11-13 18:29:36 +0800

    feat: modify throttle code

--- JavaScript/throttle.js
+++ JavaScript/throttle.js
@@ -1,23 +1,17 @@
 /**
  * 实现一个节流函数
  */
-function throttle(func, wait, options) {
+ // option: leading：false 表示禁用第一次执行 trailing: false 表示禁用停止触发的回调
+function throttle(func, wait, options = {}) {
   var timeout, context, args;
   var previous = 0;
-
-  if (!options) options = {};
-
-  var later = function() {
-    previous = options.leading === false ? 0 : new Date().getTime();
-    timeout = null;
-    func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
 
   var throttled = function() {
     var now = new Date().getTime();
 
-    if (!previous && options.leading === false) previous = now;
+    if (!previous && options.leading === false) {
+      previous = now;
+    }
 
     var remaining = wait - (now - previous);
 
@@ -36,7 +30,12 @@ function throttle(func, wait, options) {
       
       if (!timeout) context = args = null;
     } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
+      timeout = setTimeout(function() {
+        previous = options.leading === false ? 0 : new Date().getTime();
+        timeout = null;
+        func.apply(context, args);
+        if (!timeout) context = args = null;
+      }, remaining);
     }
   };
 
